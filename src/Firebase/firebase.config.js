d6cc8727bfbbb2bec16f8457ca6e612cd7df7882
unknown <imtiajahamed6161@gmail.com>
2021-12-14 05:24:47 -0800

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyDGy4Gv6Owc2zViFWlKIbDr4-AxhOgfFpk",
+    authDomain: "ema-jonh.firebaseapp.com",
+    projectId: "ema-jonh",
+    storageBucket: "ema-jonh.appspot.com",
+    messagingSenderId: "415284946771",
+    appId: "1:415284946771:web:fa56e07a22b27f69fc63a6"
+  };
+
+export default firebaseConfig;
